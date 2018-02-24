1189ce422b5736461e5984058bbad00482cc8777
Wojciechr7 <hr5t5@windowslive.com>
2018-02-24 17:58:03 +0100

    implement websockets

--- arduino_client/arduino_client.ino
+++ arduino_client/arduino_client.ino
@@ -1,50 +1,63 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
 #include <ArduinoJson.h>
-#include <typeinfo>
 
+#include <SocketIoClient.h>
 
-const char* ssid = "";
-const char* password = "";
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+SocketIoClient webSocket;
 
 int bluePin = 12;
 int redPin = 13;
- 
-void setup () {
- 
-  pinMode(bluePin, OUTPUT);
-  pinMode(redPin, OUTPUT);
- 
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
- 
-    delay(1000);
-    Serial.print("Connecting..");
- 
-  }
- 
+int switchPin = 0;
+
+void event(const char payload[], size_t length) {
+  DynamicJsonBuffer jsonBuffer(length);
+  JsonArray& root = jsonBuffer.parseArray(payload);
+
+  if (root[0]["status"] == true) {
+        digitalWrite(bluePin, HIGH);
+      } else {
+        digitalWrite(bluePin, LOW);
+      }
+
+      if (root[1]["status"] == true) {
+        digitalWrite(redPin, HIGH);
+      } else {
+        digitalWrite(redPin, LOW);
+      }
 }
 
-void loop() {
- 
-  if (WiFi.status() == WL_CONNECTED) {
- 
-    HTTPClient http; 
- 
-    int beginResult = http.begin("http://192.168.0.103:3000/leds");
-    int httpCode = http.GET();   
-          
-    Serial.println(httpCode);
-    if (httpCode > 0) {
+void setup() {
+  pinMode(bluePin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(switchPin, INPUT_PULLUP);
 
-      
-      const size_t bufferSize = JSON_ARRAY_SIZE(2) + 2*JSON_OBJECT_SIZE(5) + 140;
+
+    USE_SERIAL.begin(115200);
+
+    WiFi.begin("Hemoglobina", "zxasqw12");
+
+    while (WiFi.status() != WL_CONNECTED) {
+
+    delay(1000);
+    Serial.print("Connecting..");
+
+  }
+
+  HTTPClient http;
+
+    int beginResult = http.begin("http://192.168.0.101:3000/leds");
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      const size_t bufferSize = JSON_ARRAY_SIZE(2) + 2 * JSON_OBJECT_SIZE(5) + 140;
       DynamicJsonBuffer jsonBuffer(bufferSize);
       JsonArray& root = jsonBuffer.parseArray(http.getString());
 
-      // Parameters
       if (root[0]["status"] == true) {
         digitalWrite(bluePin, HIGH);
       } else {
@@ -58,10 +71,40 @@ void loop() {
       }
 
     }
- 
     http.end();
- 
-  }
-  delay(200);
- 
+
+    USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+      for(uint8_t t = 4; t > 0; t--) {
+          USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+          USE_SERIAL.flush();
+          delay(1000);
+      }
+
+    WiFiMulti.addAP("ssid", "pass");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+    }
+
+
+
+
+    webSocket.on("event", event);
+    webSocket.begin("192.168.0.101", 3000);
+
+}
+
+void loop() {
+    webSocket.loop();
+
+    if (digitalRead(0) == LOW) {
+      webSocket.emit("button 0");
+
+    }
+
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,18 @@
 var leds;
+var socket;
+
+var URL = 'http://192.168.0.101:3000/leds';
 
 $(document).ready(function() {
-
+    socket = io();
     reloadLeds();
+
+    socket.on('update browser', function(){
+        reloadLeds();
+    });
+    socket.on('switch leds', function(){
+        switchLeds();
+    });
 
 });
 
@@ -15,31 +25,51 @@ function reloadLeds() {
         type: 'GET',
         data: JSON.stringify(data),
         contentType: 'application/json',
-        url: 'http://localhost:3000/leds',
+        url: URL,
         success: function(data) {
             leds = data;
             $('.led-red').remove();
             $('.led-blue').remove();
             for (var led of data) {
-                $('.leds-container').append('<div onclick="updateLed(' + "'" + led.id + "'" + ', ' + "'" + led.status + "'" + ', ' + "'" + led._id + "'" + ')" class="led-' + led.color + '  ' + led.status + '" id="'+led.id+'"></div>');
+               $('.leds-container').append('<div onclick="updateLed(' + "'" + led.id + "'" + ', ' + "'" + led.status + "'" + ', ' + "'" + led._id + "'" + ')" class="led-' + led.color + '  ' + led.status + '" id="'+led.id+'"></div>');
             }
         }
     });
 }
 
+function switchLeds() {
+    leds[0].status = !leds[0].status;
+    leds[1].status = !leds[1].status;
+    socket.emit('event', leds);
+    for (var i = 0; i <= 1; i++) {
+        $.ajax({
+            type: 'PUT',
+            data: JSON.stringify(leds[i]),
+            contentType: 'application/json',
+            url: URL + '/' + leds[i]._id,
+            success: function(data) {
+                if (--i == 1) {
+                    socket.emit('update browser');
+                }
+            }
+        });
 
+    }
+
+}
 
 
 function updateLed(id, status, dbId) {
     leds[id].status = (status == 'true') ? false : true;
+    socket.emit('event', leds);
     var data = leds[id];
     $.ajax({
         type: 'PUT',
         data: JSON.stringify(data),
         contentType: 'application/json',
-        url: 'http://localhost:3000/leds/' + dbId,
+        url: URL + '/' + dbId,
         success: function(data) {
-            reloadLeds();
+            socket.emit('update browser');
         }
     });
 
